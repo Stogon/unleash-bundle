5206e035aef43214aa6495232c0e12787a58d35a
Joël Sunier <jsunier@stogon.io>
2021-09-14 20:09:13 +0200

    fix(strategy): #8 UserWithIdStrategy not working with int IDs
    added unit tests for UserWithIdStrategy

--- src/Strategy/UserWithIdStrategy.php
+++ src/Strategy/UserWithIdStrategy.php
@@ -19,27 +19,27 @@ class UserWithIdStrategy implements StrategyInterface
 			$currentUser = $context['user'];
 
 			if (is_string($currentUser)) {
-				return in_array($currentUser, $ids, true);
+				return in_array($currentUser, $ids, false);
 			}
 
-			if (method_exists($currentUser, 'getId') && in_array($currentUser->getId(), $ids, true)) {
+			if (method_exists($currentUser, 'getId') && in_array($currentUser->getId(), $ids, false)) {
 				return true;
 			}
 
 			if ($currentUser instanceof UserInterface) {
 				if (Kernel::VERSION_ID >= 50300 && method_exists($currentUser, 'getUserIdentifier')) {
-					return in_array($currentUser->getUserIdentifier(), $ids, true);
+					return in_array($currentUser->getUserIdentifier(), $ids, false);
 				}
 
-				return in_array($currentUser->getUsername(), $ids, true);
+				return in_array($currentUser->getUsername(), $ids, false);
 			}
 
-			return in_array((string) $currentUser, $ids, true);
+			return in_array((string) $currentUser, $ids, false);
 		}
 
 		/** @var Request */
 		$request = $context['request'];
 
-		return in_array($request->getUser(), $ids, true);
+		return in_array($request->getUser(), $ids, false);
 	}
 }
---
+++ tests/Strategy/UserWithIdStrategyTest.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Stogon\UnleashBundle\Tests\Strategy;
+
+use PHPUnit\Framework\TestCase;
+use Stogon\UnleashBundle\Strategy\UserWithIdStrategy;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @coversDefaultClass \Stogon\UnleashBundle\Strategy\UserWithIdStrategy
+ */
+class UserWithIdStrategyTest extends TestCase
+{
+	/**
+	 * @dataProvider usernameProvider
+	 * @covers ::isEnabled
+	 */
+	public function testIsEnabledWithUsername(array $parameters, string $username, bool $expected): void
+	{
+		$userMock = $this->createMock(UserInterface::class);
+		$userMock->method('getUsername')->willReturn($username);
+
+		$context = [
+			'user' => $userMock,
+		];
+
+		$strategy = new UserWithIdStrategy();
+
+		$this->assertEquals($expected, $strategy->isEnabled($parameters, $context));
+	}
+
+	/**
+	 * @dataProvider usernameProvider
+	 * @covers ::isEnabled
+	 */
+	public function testIsEnabledWithUserIdentifier(array $parameters, string $username, bool $expected): void
+	{
+		$userMock = $this->createMock(User::class);
+		$userMock->method('getUserIdentifier')->willReturn($username);
+
+		$context = [
+			'user' => $userMock,
+		];
+
+		$strategy = new UserWithIdStrategy();
+
+		$this->assertEquals($expected, $strategy->isEnabled($parameters, $context));
+	}
+
+	public function usernameProvider(): array
+	{
+		return [
+			[
+				[
+					'userIds' => 'johndoe,john,doe',
+				],
+				'johndoe',
+				true,
+			],
+			[
+				[
+					'userIds' => 'johndoe,john,doe',
+				],
+				'john',
+				true,
+			],
+			[
+				[
+					'userIds' => 'johndoe,john,doe',
+				],
+				'test',
+				false,
+			],
+		];
+	}
+
+	/**
+	 * @dataProvider idProvider
+	 * @covers ::isEnabled
+	 */
+	public function testIsEnabledWithId(array $parameters, $id, bool $expected): void
+	{
+		$userMock = $this->createMock(User::class);
+		$userMock->method('getId')->willReturn($id);
+
+		$context = [
+			'user' => $userMock,
+		];
+
+		$strategy = new UserWithIdStrategy();
+
+		$this->assertEquals($expected, $strategy->isEnabled($parameters, $context));
+	}
+
+	public function idProvider(): array
+	{
+		return [
+			[
+				[
+					'userIds' => '1,2,3',
+				],
+				'1',
+				true,
+			],
+			[
+				[
+					'userIds' => '1,2,3',
+				],
+				2,
+				true,
+			],
+			[
+				[
+					'userIds' => '1,2,3',
+				],
+				'test',
+				false,
+			],
+		];
+	}
+}
